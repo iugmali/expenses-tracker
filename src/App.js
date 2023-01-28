a795ee2ee780103f19c64cccca4ea6da02b20858
Guilherme Pinto Lima de Almeida <dev@iugmali.com>
2023-01-28 14:41:27 -0300

    add states to create new expenses and styles

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import './App.css';
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
+import {useEffect, useState} from "react";
 
 const App = () => {
   const expenses = [
-    {title: 'anel de noivado', amount: 50.0, date: new Date(2020, 7, 9)},
-    {title: 'curso alura', amount: 720.0, date: new Date(2022, 0, 7)},
-    {title: 'crc', amount: 545.0, date: new Date(2023, 0, 31)}
+    {id: 'e1', title: 'anel de noivado', amount: 50.0, date: new Date(2020, 7, 9)},
+    {id: 'e2', title: 'curso alura', amount: 720.0, date: new Date(2022, 0, 7)},
+    {id: 'e3', title: 'crc', amount: 545.0, date: new Date(2023, 0, 31)}
   ]
+  const [expensesData, setExpensesData] = useState(expenses)
+  const addExpenseHandler = expense => {
+    setExpensesData(prevState => {
+      return [...prevState, expense]
+    })
+  }
+  useEffect(() => {
+
+  }, [addExpenseHandler])
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <Expenses items={expenses} />
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expensesData} />
     </div>
   );
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,14 +1,18 @@
 import './Expenses.css'
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
   return (
-    <Card className="expenses">
-      { props.items.map( item => (
-        <ExpenseItem {...item} />
-      )) }
-    </Card>
+    <>
+      <ExpensesFilter />
+      <Card className="expenses">
+        { props.items.map( item => (
+          <ExpenseItem {...item} />
+        )) }
+      </Card>
+    </>
   )
 }
 
